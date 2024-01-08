d268a64cacbda02e7612dca8867b2690c3ec8138
Luc-Sauleau <135120901+Luc-Sauleau@users.noreply.github.com>
2024-01-08 14:51:25 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import numpy as np
 from skimage.io import imread
 import pylab as pl
+
+os.system('git clone https://github.com/aplyer/gefolki.git')
+sys.path.append('gefolki/python')
+from algorithm import EFolki
 
 os.system('wget https://raw.githubusercontent.com/aplyer/gefolki/master/datasets/QB.tif')
 os.system('wget https://raw.githubusercontent.com/aplyer/gefolki/master/datasets/WV.tif')
@@ -17,3 +22,17 @@ QB = imread("QB.tif")
 pl.figure()
 pl.imshow(QB,vmin=0,vmax=500,cmap='gray')
 pl.title('Image QB')
+
+mask = WV>0
+WV = WV.astype(np.float32)
+QB = QB.astype(np.float32)
+WV=WV*mask
+
+u, v = EFolki(WV, QB, iteration=4, radius=[16,8], rank=4, levels=5)
+QB=QB*mask
+
+N = np.sqrt(u**2+v**2)
+pl.figure()
+pl.imshow(N,vmin=0,vmax=60)
+pl.title('Norm of Optic to optic registration')
+pl.colorbar()

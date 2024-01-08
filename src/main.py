cdd6da6ea24ac42ba105c900d80e25d1601c633b
Luc-Sauleau <135120901+Luc-Sauleau@users.noreply.github.com>
2024-01-08 14:41:02 +0100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('wget https://raw.githubusercontent.com/aplyer/gefolki/master/datasets/QB.tif')
+os.system('wget https://raw.githubusercontent.com/aplyer/gefolki/master/datasets/WV.tif')
